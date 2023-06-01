7bc86344d315ebaa8921fdaa0165c58c23f0c43f
Gabriel Migliatti <101683285+Migliatti@users.noreply.github.com>
2023-06-01 13:37:38 -0300

    retirando arquivo de bug(provavelmente)

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./reset.css";
 import "./index.css";
-import App from "./app";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import MyPlant from "./components/MyPlant";
+import Ofertas from "./components/Ofertas";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Hero />
+    <MyPlant />
+    <Ofertas />
   </React.StrictMode>
 );
